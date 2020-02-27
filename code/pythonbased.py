814c96e6c613fd612a5a07ebf8bc18a23ccc52c3
NemoNemo03 <lixiaohua_03@163.com>
2020-02-27 14:18:49 +0800

    with pythonbased.py

---
+++ code/pythonbased.py
@@ -0,0 +1,26 @@
+import pickle
+d = dict(name='李小虎', age=29, score=80)
+str = pickle.dumps(d)
+print(str)
+file2 = open("out5.txt","w")
+
+import json
+d1 = dict(name='李小虎', age=29, score=80)
+str = json.dumps(d1)
+print(str)
+d2 = json.loads(str)
+print(d2)
+
+#for line in open("input.txt"):
+#    file2.write('"'+line+'"'+",")
+#   pass
+
+#with open('input.txt' , 'r') as f:
+ #   for line in f:
+  #      print(line)
+#import os
+#print(os.environ)
+#print(os.path.abspath('.'))
+
+
+
